37b7241f3fb03cbc3294b1b80d994f7082535cf2
charan-creator-maker <cherrycharan18450@gmail.com>
2025-04-23 14:30:08 +0530

    Update helloserverlet.java

--- helloserverlet.java
+++ helloserverlet.java
@@ -1,14 +1,13 @@
-package com.example;
+package com.example.web;
 
+import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.io.*;
 
 public class HelloServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello from Java Servlet!</h1>");
+        response.getWriter().println("<h2>Hello from Servlet!</h2>");
     }
 }
